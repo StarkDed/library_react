e2821779c67ff0b931a6efe8b690728d298fa1d3
Denis Starkov <starkden1000@gmail.com>
2025-06-10 13:42:47 +0300

    feat: add book list component structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import { useState } from "react";
+
 import "./styles/app.css";
 import Header from "./сomponents/Header.jsx";
+import BookList from "./сomponents/BookList.jsx";
 
 const App = () => {
+  const [listBooks, setListBooks] = useState([]);
+
   return (
     <div className="app">
       <Header />
+      <BookList listBooks={listBooks} />
     </div>
   );
 };
---
+++ src/сomponents/BookList.jsx
@@ -0,0 +1,15 @@
+import "../styles/BookList.css";
+import { useState } from "react";
+
+const BookList = ({ listBooks }) => {
+  const [headline, setHeadline] = useState("Лучшие книги");
+
+  return (
+    <div className="book-list-wrapper">
+      <h2>{headline}</h2>
+      <div className="book-list"></div>
+    </div>
+  );
+};
+
+export default BookList;
