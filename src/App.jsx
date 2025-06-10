bd956008445f4d6449d6f3d53f41e452bc82ffca
Denis Starkov <starkden1000@gmail.com>
2025-06-10 14:57:53 +0300

    feat: add Footer structure and layout fixed

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import "./styles/app.css";
 import Header from "./сomponents/Header.jsx";
 import BookList from "./сomponents/BookList.jsx";
+import Footer from "./сomponents/Footer.jsx";
 
 const App = () => {
   const [listBooks, setListBooks] = useState([]);
@@ -11,6 +12,7 @@ const App = () => {
     <div className="app">
       <Header />
       <BookList listBooks={listBooks} />
+      <Footer></Footer>
     </div>
   );
 };
---
+++ src/сomponents/Footer.jsx
@@ -0,0 +1,7 @@
+import "../styles/footer.css";
+
+const Footer = () => {
+  return <footer></footer>;
+};
+
+export default Footer;
