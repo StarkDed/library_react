ff2c8afe915bfd59395a3660b862ac4ce7aa7963
Denis Starkov <starkden1000@gmail.com>
2025-06-11 11:13:06 +0300

    refactor: extract search into React component

--- src/сomponents/Header/Header.jsx
+++ src/сomponents/Header/Header.jsx
@@ -1,5 +1,5 @@
 import styles from "./header.module.css";
-import SortingSvg from "../../assets/svg/SortingSvg/SortingSvg.jsx";
+import Search from "../Search/Search.jsx";
 
 const Header = () => {
   return (
@@ -7,11 +7,7 @@ const Header = () => {
       <div className={styles["header-content"]}>
         <h1>Library</h1>
         <button className={styles["btn-catalog"]}>Каталог</button>
-        <div className={styles["search-container"]}>
-          <input placeholder="Поиск" type="search" />
-          <button className={styles["btn-search"]}>Найти</button>
-          <SortingSvg className={styles["sorting-icon"]} />
-        </div>
+        <Search />
         <button className={styles["btn-login"]}>Войти</button>
       </div>
     </header>
