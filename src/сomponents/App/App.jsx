2e03f6a5dcd4fefe54835e266e2b9982708cc4e6
Denis Starkov <starkden1000@gmail.com>
2025-06-12 10:30:07 +0300

    feat: created structure of Catalog

--- src/сomponents/App/App.jsx
+++ src/сomponents/App/App.jsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
 
 import "./app.css";
+
 import Header from "../Header/Header.jsx";
 import BookList from "../BookList/BookList.jsx";
 import Footer from "../Footer/Footer.jsx";
+import Catalog from "../Catalog/Catalog.jsx";
 
 const App = () => {
   const [listBooks, setListBooks] = useState([
@@ -76,11 +78,19 @@ const App = () => {
     },
   ]);
 
+  const [isCatalog, setIsCatalog] = useState(true);
+
   return (
     <div className="app">
       <Header />
-      <BookList listBooks={listBooks} />
-      <Footer></Footer>
+      {isCatalog ? (
+        <Catalog />
+      ) : (
+        <>
+          <BookList listBooks={listBooks} />
+          <Footer></Footer>
+        </>
+      )}
     </div>
   );
 };
---
+++ src/сomponents/Catalog/Catalog.jsx
@@ -0,0 +1,11 @@
+import styles from "./catalog.module.css";
+
+const Catalog = () => {
+  return (
+    <div className={styles.catalog}>
+      <h1>Hello Catalog</h1>
+    </div>
+  );
+};
+
+export default Catalog;
